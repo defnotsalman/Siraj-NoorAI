2def2170578c61d0ad552cc2a18a6966d079dd72
Muhammad Salman <salmaniscool31@gmail.com>
2026-07-20 11:20:12 +0500

    fix(client): Inject dynamic host redirection patch in main.jsx for mobile device testing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,6 +5,49 @@ import { ThemeProvider } from "./context/ThemeContext";
 import App from "./App.jsx";
 import "./styles/global.css";
 
+// Global URL rewriting patch to support testing on physical mobile phones
+(function() {
+  const getApiBase = () => {
+    const envUrl = import.meta.env.VITE_API_URL;
+    if (envUrl && !envUrl.includes("localhost") && !envUrl.includes("127.0.0.1")) {
+      return envUrl;
+    }
+    // Dynamically fallback to the host IP of the machine serving the site
+    return `http://${window.location.hostname}:5000`;
+  };
+
+  // Intercept fetch API requests
+  const originalFetch = window.fetch;
+  window.fetch = function (input, init) {
+    if (typeof input === 'string') {
+      const apiBase = getApiBase();
+      input = input.replace('http://localhost:5000', apiBase)
+                   .replace('http://127.0.0.1:5000', apiBase);
+    }
+    return originalFetch(input, init);
+  };
+
+  // Intercept media source bindings (Audio/Video tags)
+  const originalSrcDescriptor = Object.getOwnPropertyDescriptor(HTMLMediaElement.prototype, 'src');
+  if (originalSrcDescriptor) {
+    Object.defineProperty(HTMLMediaElement.prototype, 'src', {
+      set: function(val) {
+        if (typeof val === 'string') {
+          const apiBase = getApiBase();
+          val = val.replace('http://localhost:5000', apiBase)
+                   .replace('http://127.0.0.1:5000', apiBase);
+        }
+        originalSrcDescriptor.set.call(this, val);
+      },
+      get: function() {
+        return originalSrcDescriptor.get.call(this);
+      },
+      configurable: true,
+      enumerable: true
+    });
+  }
+})();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider>
